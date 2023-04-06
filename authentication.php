775babd4d91f4f55e3990693bb7256be943952be
Costin Iordache <71375853+papicosti@users.noreply.github.com>
2023-04-06 22:43:56 +0300

    Update authentication.php

--- authentication.php
+++ authentication.php
@@ -9,9 +9,7 @@
         $count = mysqli_num_rows($result);  
           
         if($count == 1){  
-            echo "<h1><center> Login successful </center></h1>";  
         }  
-        else{  
-            echo "<h1> Login failed. Invalid username or password.</h1>";  
+        else{   
         }     
 ?> 
